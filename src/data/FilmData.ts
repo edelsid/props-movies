ec533a81c210e4071b59a4dcd1eb577fc5278ffa
Xenia Ch <eradonna@yandex.ru>
2023-12-22 02:15:19 +0300

    fixing image paths

--- src/data/FilmData.ts
+++ src/data/FilmData.ts
@@ -1,7 +1,10 @@
 import type { Film } from "./models/Film";
+import act from "../img/act.jpg";
+import rom from "../img/rom.jpg";
+import scifi from "../img/scifi.jpg";
 
 export const filmData: Film[] = [
-   {id: 1, title: 'Матрица: Перезагрузка', rating: 4, price: 950, favorite: true, genre: 'экшн', img: '/img/act.jpg'},
-   {id: 2, title: 'Титаник', rating: 5, price: 500, favorite: false, genre: 'романтика', img: '/img/rom.jpg'},
-   {id: 3, title: 'Гравитация', rating: 0, price: 800, favorite: false, genre: 'фантастика', img: '/img/scifi.jpg'}
+   {id: 1, title: 'Матрица: Перезагрузка', rating: 4, price: 950, favorite: true, genre: 'экшн', img: act},
+   {id: 2, title: 'Титаник', rating: 5, price: 500, favorite: false, genre: 'романтика', img: rom},
+   {id: 3, title: 'Гравитация', rating: 0, price: 800, favorite: false, genre: 'фантастика', img: scifi}
 ]
